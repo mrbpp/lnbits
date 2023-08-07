f5eb476c06ce2fdfed9e577840b430ef5fcf3fdf
dni ⚡ <office@dnilabs.com>
2023-08-07 14:45:53 +0200

    Update lnbits/bolt11.py
    Co-authored-by: jackstar12 <62219658+jackstar12@users.noreply.github.com>
    
    fixup

--- lnbits/bolt11.py
+++ lnbits/bolt11.py
@@ -1,5 +1,3 @@
 from bolt11.decode import decode  # noqa: F401
 from bolt11.encode import encode  # noqa: F401
-from bolt11.types import Bolt11
-
-Invoice = Bolt11
+from bolt11.types import Bolt11 as Invoice  # noqa: F401
