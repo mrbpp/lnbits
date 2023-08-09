41a17554c290962706fc661ab49af1adf4cb57ba
dni ⚡ <office@dnilabs.com>
2023-08-10 00:39:56 +0200

    dont forget to add core_app

--- lnbits/core/__init__.py
+++ lnbits/core/__init__.py
@@ -3,10 +3,10 @@ from fastapi import APIRouter
 from .db import core_app_extra, db  # noqa: F401
 from .views.admin_api import admin_router
 from .views.api import api_router
+
+# this compat is needed for usermanager extension
 from .views.generic import update_user_extension  # noqa: F401
-from .views.generic import (  # this compat is needed for usermanager extension
-    generic_router,
-)
+from .views.generic import generic_router
 from .views.public_api import public_router
 
 # backwards compatibility for extensions
@@ -14,7 +14,8 @@ core_app: APIRouter = APIRouter()
 
 
 def init_core_routers(app):
-    app.include_router(generic_router)  # noqa: F405
-    app.include_router(public_router)  # noqa: F405
-    app.include_router(api_router)  # noqa: F405
-    app.include_router(admin_router)  # noqa: F405
+    app.include_router(core_app)
+    app.include_router(generic_router)
+    app.include_router(public_router)
+    app.include_router(api_router)
+    app.include_router(admin_router)
